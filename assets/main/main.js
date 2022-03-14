776983b075e3fa12f705a6ffaf1f4ac4da1db737
Duy Toan <toanpmo123@gmail.com>
2022-03-14 10:35:58 +0700

    up
    10:35

--- assets/main/main.js
+++ assets/main/main.js
@@ -1,14 +1,1 @@
 
-      $('.listing-sp-1555572254399 .product-wrapper .thumb-image img').on('click', function(e){
-          var imgUrl = $(this).data("swatch-image"),
-          parent = $(this).parents('.product-wrapper'),
-          imgElem = parent.find('.product-image img.change-image');
-      
-          if(imgElem.hasClass('lazyloaded')){
-            imgElem.attr('data-srcset',$(this).attr('data-srcset'));
-            imgElem.attr('srcset',$(this).attr('srcset'));
-          }
-          else{
-            imgElem.attr('src', imgUrl);
-          }
-      });  
